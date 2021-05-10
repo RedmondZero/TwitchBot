44473c8270161f32a87c92fc58c7eedade4557e4
RedmondZero <83933404+RedmondZero@users.noreply.github.com>
2021-05-10 16:24:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const tmi = require('tmi.js'),
     client.connect().catch(console.error);
     
     client.on('connected', () => {
-        client.say(channel, 'tennessedanielsbot has connected! ');
+        client.say(channel, 'tennessedaneilsbot has connected! ');
     });
 
     client.on('message', (channel, user, message, self) => {
@@ -27,15 +27,20 @@ const tmi = require('tmi.js'),
         if(message == '!hello') {
             client.say(channel, `@${user.username}, hello!`);
         }
-        if(message ==  '!poggers') {
-            client.say(channel, `nmpPOGGERS nmpPOGGERS nmpPOGGERS 
-            nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS 
-            nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS 
-            nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS 
-            nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS 
-            nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS `);
+        if(message ==  '!test') {
+            client.say(channel, `MrDestructoid Online`);
         }
-        if(message == '!byebot') {
-            client.part(channel, 'Bye Chat');
+        if(message ==  '!poggers') {
+            client.say(channel, `nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS 
+            nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS 
+            nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS nmpPOGGERS `);
+        }
+        if(message ==  '!nuggies') {
+            client.say(channel, `nmpNuggies nmpNuggies nmpNuggies nmpNuggies nmpNuggies nmpNuggies 
+            nmpNuggies nmpNuggies nmpNuggies nmpNuggies nmpNuggies nmpNuggies nmpNuggies nmpNuggies `);
+        }
+        if(message == '!bye') {
+            client.say(channel, 'Bye Chat');
+            client.part(channel,);
         }
     });
